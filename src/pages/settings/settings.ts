fdcaf438b7cfb0aa0e6ea6cf19a597c5b1190cc2
Gustavo Fernandes <gus@Gustavos-MacBook-Pro.local>
2017-04-29 20:22:01 -0400

    Settings now working to update major and year

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { NavController, NavParams, AlertController, LoadingController } from 'ionic-angular';
+import { NavController, NavParams, AlertController, LoadingController, ViewController, ToastController, Keyboard } from 'ionic-angular';
 import { Settings } from '../../providers/settings';
 import { TranslateService } from 'ng2-translate/ng2-translate';
 import { PasswordPage } from '../password/password';
@@ -21,6 +21,10 @@ import gql from 'graphql-tag';
 export class SettingsPage {
   // Our local settings object
   currentUser = <any>{};
+  major: any;
+  year: any;
+
+  data: any;
 
   loading: any;
   constructor(public navCtrl: NavController,
@@ -30,37 +34,74 @@ export class SettingsPage {
               public translate: TranslateService,
               private alertCtrl: AlertController,
               public loadingCtrl: LoadingController,
-              private apollo: Angular2Apollo ) {
-  }
-  ngOnInit() {
+              private apollo: Angular2Apollo,
+              public viewCtrl: ViewController,
+              public toastCtrl: ToastController,
+              public keyboard: Keyboard )
+  {
     this.currentUserInfo().then(({data}) => {
       this.currentUser = data;
       this.currentUser = this.currentUser.user;
-      // console.log(this.currentUser);
+      this.major = this.currentUser.major;
+      this.year = this.currentUser.year;
     });
   }
 
+  userQuery = gql`
+    query{
+      user{
+        id
+        email
+        major
+        year
+      }
+    }
+  `;
+
+  ionViewDidEnter() {
+    this.data = this.apollo.watchQuery({query: this.userQuery});
+    this.data.refetch().then(({data}) => {
+      this.currentUser = data;
+      this.currentUser = this.currentUser.user;
+      this.major = this.currentUser.major;
+      this.year = this.currentUser.year;
+    });
+  }
 
   currentUserInfo(){
       return this.apollo.query({
-        query: gql`
-          query{
-            user{
-              id
-              email
-            }
-          }
-        `
+        query: this.userQuery
       }).toPromise();
     }
 
-  ngOnChanges() {
-    console.log('Ng All Changes');
+  cancel() {
+    this.viewCtrl.dismiss();
   }
 
-  saveChages() {
+  save() {
+    this.apollo.mutate({
+      mutation: gql`
+      mutation updateUser($id: ID!, $major: String, $year: String) {
+        updateUser(id:$id, major:$major, year: $year) {
+          id
+        }
+      }
+      `, variables: {
+        id: this.currentUser.id,
+        major: this.major,
+        year: this.year
+      }
+    }).toPromise();
+    let toast = this.toastCtrl.create({
+      message: 'Changes saved!',
+      position: 'top',
+      duration: 3000
+    });
+    toast.present();
 
+    this.viewCtrl.dismiss();
   }
+
   changePassword(){
     this.navCtrl.push(PasswordPage);
   }
@@ -75,6 +116,10 @@ export class SettingsPage {
       location.reload();
       this.navCtrl.push(WelcomePage);
       //  this.app.getRootNav().setRoot(WelcomePage);
-    }
+  }
+
+  closeKeyboard() {
+    this.keyboard.close();
+  }
 
 }
