a7a01e5e8cbb206a26140c824ecc730ef0c77f20
Simeon Goolsby <goolsbysim@gmail.com>
2017-05-01 22:06:38 -0400

    Fixed duplicate mocks

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -5,6 +5,9 @@ import { SetLocationPage } from '../set-location/set-location';
 
 import * as Leaflet from 'leaflet';
 
+// This global variable makes me sad
+var mocks = false;
+
 /*
   Generated class for the Map page.
 
@@ -47,7 +50,73 @@ export class MapPage implements OnInit{
     }
 
     function addMockPins(e) {
-      var radius = e.accuracy / 4;
+      if(mocks == false) {
+        var radius = e.accuracy / 4;
+        var profileIcon = Leaflet.icon({
+          iconUrl: 'https://cdn0.iconfinder.com/data/icons/education-15/500/reader-512.png',
+          iconSize: [38, 38] // size of the icon
+        });
+
+        let pins = [
+          {
+            class: 'CHEM 141',
+            time: '2pm - 5pm',
+            offset: [0.0001, 0.0007],
+            images: ['<img src="http://i1246.photobucket.com/albums/gg611/theofficechic/Design/profile-round.png" style="width:20%;height:20%;">']
+
+          },
+          {
+            class: 'MTH 234',
+            time: '10am - 12pm',
+            offset: [0.0008, 0.0003],
+            images: [
+              '<img src="http://ablissfulhaven.com/dev/wp-content/uploads/2015/06/round-chokolatta-profile-pic.png" style="width:20%;height:20%;">',
+              '<img src="http://s3-us-west-2.amazonaws.com/s.cdpn.io/6083/profile/profile-512_1.jpg" style="width:20%;height:20%;">'
+            ]
+          },
+          {
+            class: 'FI 320',
+            time: '8pm - 2am',
+            offset: [0.0004, -0.0005],
+            images: [
+              '<img src="https://rigorous-digital.co.uk/wp-content/uploads/2014/06/profile-round.png" style="width:20%;height:20%;">',
+              '<img src="https://learn.neighborly.com/wp-content/uploads/2015/03/sean_profile_picture_round.png" style="width:20%;height:20%;">',
+              '<img src="http://www.imperfectlyhis.com/wp-content/uploads/2016/01/round-profile.png" style="width:20%;height:20%;">'
+            ]
+          },
+          {
+            class: 'IAH 201',
+            time: '5pm - 8pm',
+            offset: [-0.0006, -0.0001],
+            images: [
+              '<img src="http://www.ruchikabehal.com/wp-content/uploads/2013/12/Profile-round-shot.jpg" style="width:20%;height:20%;">',
+              '<img src="http://helpgrowchange.com/wp-content/uploads/2014/03/tb_profile_201303_round.png" style="width:20%;height:20%;">',
+              '<img src="http://2.bp.blogspot.com/-mQqw7TAQvW4/VX4vn65C9TI/AAAAAAAABDQ/lrrNssmxc-g/s290/round%2Bprofile1.png" style="width:20%;height:20%;">'
+            ]
+          },
+        ];
+        for(var i=0; i<4; i++) {
+          let num = pins[i]['images'].length;
+          let desc = '<h5 style="text-align:center;">' + pins[i]['class'] + '</h5>';
+          desc += '<p class="centerb">' + pins[i]['time'] + '</p>';
+          desc += '<p><em>(' + num +') People studying:</em></p>';
+          desc += '<p class="centerb">';
+          for(var j=0; j<num; j++) {
+            desc += pins[i]['images'][j];
+          }
+          desc += '</p>';
+          desc += '<p class="centerb"><button ion-button style="color:white;background-color:red;">Ask to Join</button></p>';
+
+          let new_lat = e.latlng.lat + pins[i]['offset'][0];
+          let new_lng = e.latlng.lng + pins[i]['offset'][1];
+          let latlng = Leaflet.latLng(new_lat, new_lng);
+
+          Leaflet.marker(latlng, {icon: profileIcon}).addTo(map)
+              .bindPopup(desc);//.openPopup();
+        }
+        mocks = true;
+      }
+      /*var radius = e.accuracy / 4;
       var profileIcon = Leaflet.icon({
         iconUrl: 'https://cdn0.iconfinder.com/data/icons/education-15/500/reader-512.png',
         iconSize: [38, 38] // size of the icon
@@ -110,6 +179,7 @@ export class MapPage implements OnInit{
         Leaflet.marker(latlng, {icon: profileIcon}).addTo(map)
             .bindPopup(desc);//.openPopup();
       }
+      mocks = true;*/
     }
   }
 
