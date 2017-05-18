7e98004ded220e83d29765a82862b6d0568cc9cb
Gustavo Fernandes <gus@Gustavos-MacBook-Pro.local>
2017-05-18 14:33:40 -0400

    Map works with databasegit add src/pages/map/map.ts

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -2,36 +2,51 @@ import { Component, OnInit } from '@angular/core';
 import { NavController, NavParams, ModalController } from 'ionic-angular';
 import { SetLocationPage } from '../set-location/set-location';
 
+//Graphcool
+import { Angular2Apollo } from 'angular2-apollo';
+import { Subscription } from 'rxjs/Subscription'
+import gql from 'graphql-tag';
+import 'rxjs/add/operator/toPromise';
+
 
 import * as Leaflet from 'leaflet';
 
 // This global variable makes me sad
 var mocks = false;
 
-/*
-  Generated class for the Map page.
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-map',
   templateUrl: 'map.html'
 })
-export class MapPage implements OnInit{
+export class MapPage implements OnInit {
   map: any;
+  locationInfo = <any>{};
   filterBy:string = "all";
+  alllocations: any;
+
+  currentUser = <any>{};
   //class: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public modalCtrl: ModalController) {
-    //this.class = this.navParams.get('class');
-    //console.log(this.class);
+  constructor(public navCtrl: NavController, public navParams: NavParams, public modalCtrl: ModalController, public apollo: Angular2Apollo) {
+    this.getAllPins().then(({data})=> {
+      this.alllocations = data;
+      this.alllocations = this.alllocations.allMapPinses;
+    })
+    this.currentUserInfo().then(({data}) => {
+      if (data){
+        this.currentUser = data;
+        this.currentUser = this.currentUser.user;
+        // console.log(this.currentUser);
+      }
+    })
   }
 
   ngOnInit(): void {
     this.drawMap();
   }
   drawMap(): void {
+    var that = this;
     let map = Leaflet.map('map');
     this.map = map;
     Leaflet.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoicGF0cmlja3IiLCJhIjoiY2l2aW9lcXlvMDFqdTJvbGI2eXUwc2VjYSJ9.trTzsdDXD2lMJpTfCVsVuA', {
@@ -41,147 +56,30 @@ export class MapPage implements OnInit{
 
     //web location
     this.map.locate({ setView: true});
-    this.map.on('locationfound', addMockPins);
+    this.map.on('locationfound', function addMockPins(e) {
+      let pins: any;
+      that.getAllPins().then(({data})=> {
+        this.alllocations = data;
+        this.alllocations = this.alllocations.allMapPinses;
 
-    function getRandomInt(min, max) {
-      min = Math.ceil(min);
-      max = Math.floor(max);
-      return Math.floor(Math.random() * (max - min)) + min;
-    }
-
-    function addMockPins(e) {
-      if(mocks == false) {
         var radius = e.accuracy / 4;
         var profileIcon = Leaflet.icon({
-          iconUrl: 'https://cdn0.iconfinder.com/data/icons/education-15/500/reader-512.png',
+          iconUrl: 'https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678093-pin-128.png',
           iconSize: [38, 38] // size of the icon
         });
-
-        let pins = [
-          {
-            class: 'CHEM 141',
-            time: '2pm - 5pm',
-            offset: [0.0001, 0.0007],
-            images: ['<img src="http://i1246.photobucket.com/albums/gg611/theofficechic/Design/profile-round.png" style="width:20%;height:20%;">']
-
-          },
-          {
-            class: 'MTH 234',
-            time: '10am - 12pm',
-            offset: [0.0008, 0.0003],
-            images: [
-              '<img src="http://ablissfulhaven.com/dev/wp-content/uploads/2015/06/round-chokolatta-profile-pic.png" style="width:20%;height:20%;">',
-              '<img src="http://s3-us-west-2.amazonaws.com/s.cdpn.io/6083/profile/profile-512_1.jpg" style="width:20%;height:20%;">'
-            ]
-          },
-          {
-            class: 'FI 320',
-            time: '8pm - 2am',
-            offset: [0.0004, -0.0005],
-            images: [
-              '<img src="https://rigorous-digital.co.uk/wp-content/uploads/2014/06/profile-round.png" style="width:20%;height:20%;">',
-              '<img src="https://learn.neighborly.com/wp-content/uploads/2015/03/sean_profile_picture_round.png" style="width:20%;height:20%;">',
-              '<img src="http://www.imperfectlyhis.com/wp-content/uploads/2016/01/round-profile.png" style="width:20%;height:20%;">'
-            ]
-          },
-          {
-            class: 'IAH 201',
-            time: '5pm - 8pm',
-            offset: [-0.0006, -0.0001],
-            images: [
-              '<img src="http://www.ruchikabehal.com/wp-content/uploads/2013/12/Profile-round-shot.jpg" style="width:20%;height:20%;">',
-              '<img src="http://helpgrowchange.com/wp-content/uploads/2014/03/tb_profile_201303_round.png" style="width:20%;height:20%;">',
-              '<img src="http://2.bp.blogspot.com/-mQqw7TAQvW4/VX4vn65C9TI/AAAAAAAABDQ/lrrNssmxc-g/s290/round%2Bprofile1.png" style="width:20%;height:20%;">'
-            ]
-          },
-        ];
-        for(var i=0; i<4; i++) {
-          let num = pins[i]['images'].length;
-          let desc = '<h5 style="text-align:center;">' + pins[i]['class'] + '</h5>';
-          desc += '<p class="centerb">' + pins[i]['time'] + '</p>';
-          desc += '<p><em>(' + num +') People studying:</em></p>';
-          desc += '<p class="centerb">';
-          for(var j=0; j<num; j++) {
-            desc += pins[i]['images'][j];
-          }
-          desc += '</p>';
-          desc += '<p class="centerb"><button ion-button style="color:white;background-color:red;">Ask to Join</button></p>';
-
-          let new_lat = e.latlng.lat + pins[i]['offset'][0];
-          let new_lng = e.latlng.lng + pins[i]['offset'][1];
-          let latlng = Leaflet.latLng(new_lat, new_lng);
-
+        for(var i=0; i < this.alllocations.length; i++) {
+          console.log(this.alllocations);
+          let desc = '<h5 style="text-align:center;">' + this.alllocations[i]['sectionName'] + '</h5>';
+          desc += '<p class="centerb">' + this.alllocations[i]['startTime'] + ' - ' + this.alllocations[i]['endTime'] +'</p>';
+          let latlng = Leaflet.latLng(this.alllocations[i]['latitude'], this.alllocations[i]['longitude']);
           Leaflet.marker(latlng, {icon: profileIcon}).addTo(map)
               .bindPopup(desc);//.openPopup();
         }
-        mocks = true;
-      }
-      /*var radius = e.accuracy / 4;
-      var profileIcon = Leaflet.icon({
-        iconUrl: 'https://cdn0.iconfinder.com/data/icons/education-15/500/reader-512.png',
-        iconSize: [38, 38] // size of the icon
-      });
+      })
 
-      let pins = [
-        {
-          class: 'CHEM 141',
-          time: '2pm - 5pm',
-          offset: [0.0001, 0.0007],
-          images: ['<img src="http://i1246.photobucket.com/albums/gg611/theofficechic/Design/profile-round.png" style="width:20%;height:20%;">']
-
-        },
-        {
-          class: 'MTH 234',
-          time: '10am - 12pm',
-          offset: [0.0008, 0.0003],
-          images: [
-            '<img src="http://ablissfulhaven.com/dev/wp-content/uploads/2015/06/round-chokolatta-profile-pic.png" style="width:20%;height:20%;">',
-            '<img src="http://s3-us-west-2.amazonaws.com/s.cdpn.io/6083/profile/profile-512_1.jpg" style="width:20%;height:20%;">'
-          ]
-        },
-        {
-          class: 'FI 320',
-          time: '8pm - 2am',
-          offset: [0.0004, -0.0005],
-          images: [
-            '<img src="https://rigorous-digital.co.uk/wp-content/uploads/2014/06/profile-round.png" style="width:20%;height:20%;">',
-            '<img src="https://learn.neighborly.com/wp-content/uploads/2015/03/sean_profile_picture_round.png" style="width:20%;height:20%;">',
-            '<img src="http://www.imperfectlyhis.com/wp-content/uploads/2016/01/round-profile.png" style="width:20%;height:20%;">'
-          ]
-        },
-        {
-          class: 'IAH 201',
-          time: '5pm - 8pm',
-          offset: [-0.0006, -0.0001],
-          images: [
-            '<img src="http://www.ruchikabehal.com/wp-content/uploads/2013/12/Profile-round-shot.jpg" style="width:20%;height:20%;">',
-            '<img src="http://helpgrowchange.com/wp-content/uploads/2014/03/tb_profile_201303_round.png" style="width:20%;height:20%;">',
-            '<img src="http://2.bp.blogspot.com/-mQqw7TAQvW4/VX4vn65C9TI/AAAAAAAABDQ/lrrNssmxc-g/s290/round%2Bprofile1.png" style="width:20%;height:20%;">'
-          ]
-        },
-      ];
-      for(var i=0; i<4; i++) {
-        let num = pins[i]['images'].length;
-        let desc = '<h5 style="text-align:center;">' + pins[i]['class'] + '</h5>';
-        desc += '<p class="centerb">' + pins[i]['time'] + '</p>';
-        desc += '<p><em>(' + num +') People studying:</em></p>';
-        desc += '<p class="centerb">';
-        for(var j=0; j<num; j++) {
-          desc += pins[i]['images'][j];
-        }
-        desc += '</p>';
-        desc += '<p class="centerb"><button ion-button style="color:white;background-color:red;">Ask to Join</button></p>';
-
-        let new_lat = e.latlng.lat + pins[i]['offset'][0];
-        let new_lng = e.latlng.lng + pins[i]['offset'][1];
-        let latlng = Leaflet.latLng(new_lat, new_lng);
-
-        Leaflet.marker(latlng, {icon: profileIcon}).addTo(map)
-            .bindPopup(desc);//.openPopup();
-      }
-      mocks = true;*/
-    }
+    });
   }
+
 
   setLocation() {
     let addModal = this.modalCtrl.create(SetLocationPage);
@@ -197,38 +95,100 @@ export class MapPage implements OnInit{
     //web location
     if(c) {
       this.map.locate({ setView: true});
-      this.map.on('locationfound', onLocationFound);
+      this.map.on('locationfound', function (onLocationFound) {
+        that.createPin(c,onLocationFound.latlng.lat,onLocationFound.latlng.lng );
+        var profileIcon = Leaflet.icon({
+          iconUrl: 'https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678093-pin-128.png',
+        //iconUrl: 'https://cdn0.iconfinder.com/data/icons/industrial-icons/164/5-512.png',
+          iconSize: [38, 38], // size of the icon
+        });
+        let desc = '<h5 style="text-align:center;">' + c['name'] + '</h5>';
+        desc += '<p class="centerb">' + c['startTime'] + ' - ' + c['endTime'] +'</p>';
+        let latlng = Leaflet.latLng(onLocationFound.latlng.lat, onLocationFound.latlng.lng);
+        Leaflet.marker(latlng, {icon: profileIcon}).addTo(that.map)
+            .bindPopup(desc);//.openPopup();
+      });
     }
+  }
 
+  createPin(c, lat, lng) {
+    this.apollo.mutate({
+      mutation: gql`
+      mutation createMapPins($latitude: Float, $longitude: Float, $startTime: String, $endTime: String, $sectionName: String, $userId: ID! ){
+        createMapPins(latitude: $latitude, longitude: $longitude, startTime: $startTime, endTime: $endTime, sectionName: $sectionName, userId: $userId  ){
+          id
+        }
+      }
+      `,
+      variables: {
+        latitude: lat,
+        longitude: lng,
+        startTime: c.startTime,
+        endTime: c.endTime,
+        sectionName: c.name,
+        userId: this.currentUser.id
+      }
+    });
+  }
+
+  getAllPins() {
+    return this.apollo.query({
+      query: gql`
+        query{
+          allMapPinses{
+            id
+            startTime
+            endTime
+            latitude
+            longitude
+            sectionName
+          }
+        }
+      `
+    }).toPromise();
+  }
+
+  currentUserInfo(){
+      return this.apollo.query({
+        query: gql`
+          query{
+            user{
+              id
+            }
+          }
+        `
+      }).toPromise();
+    }
 
     //when we have a location draw a marker and accuracy circle
-    function onLocationFound(e) {
-      var radius = e.accuracy / 4;
-      var profileIcon = Leaflet.icon({
-        iconUrl: 'https://cdn0.iconfinder.com/data/icons/education-15/500/reader-512.png',
-      //iconUrl: 'https://cdn0.iconfinder.com/data/icons/industrial-icons/164/5-512.png',
-        iconSize: [38, 38], // size of the icon
-      });
+    onLocationFound(e) {
+      // var radius = e.accuracy / 4;
+      // var profileIcon = Leaflet.icon({
+      //   iconUrl: 'https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678093-pin-128.png',
+      // //iconUrl: 'https://cdn0.iconfinder.com/data/icons/industrial-icons/164/5-512.png',
+      //   iconSize: [38, 38], // size of the icon
+      // });
 
-      let desc = '<h5 style="text-align:center;">' + c.name.toUpperCase() + '</h5>';
-      //let desc = '<h5>' + c.name.toUpperCase() + '</h5>';
-      desc += '<p>'+ c.startTime+' - '+c.endTime+'</p>';
-      desc += '<p><em>(4) People studying:</em></p>';
-      desc += '<p><img src="http://i1246.photobucket.com/albums/gg611/theofficechic/Design/profile-round.png" style="width:20%;height:20%;">\
-      <img src="http://ablissfulhaven.com/dev/wp-content/uploads/2015/06/round-chokolatta-profile-pic.png" style="width:20%;height:20%;">\
-      <img src="http://s3-us-west-2.amazonaws.com/s.cdpn.io/6083/profile/profile-512_1.jpg" style="width:20%;height:20%;">\
-      <img src="https://rigorous-digital.co.uk/wp-content/uploads/2014/06/profile-round.png" style="width:20%;height:20%;"></p>';
-      desc += '<p class="buttons"><button ion-button style="color:white;background-color:red;">Ask to Join</button></p>';
-      Leaflet.marker(e.latlng, {icon: profileIcon}).addTo(that.map)
-          .bindPopup(desc);//.openPopup();
-
-      Leaflet.circle(e.latlng, radius, {color: 'red'}).addTo(that.map);
-
-
+      // var that = this;
+      // let desc = '<h5 style="text-align:center;">' + this.alllocations['sectionName'] + '</h5>';
+      // desc += '<p class="centerb">' + this.alllocations['startTime'] + ' - ' + this.alllocations['endTime'] +'</p>';
+      // let latlng = Leaflet.latLng(this.alllocations['latitude'], this.alllocations['longitude']);
+      // Leaflet.marker(latlng, {icon: profileIcon}).addTo(that.map)
+      //     .bindPopup(desc);//.openPopup();
+      // let desc = '<h5 style="text-align:center;">' + c.name.toUpperCase() + '</h5>';
+      // //let desc = '<h5>' + c.name.toUpperCase() + '</h5>';
+      // desc += '<p>'+ c.startTime+' - '+c.endTime+'</p>';
+      // desc += '<p><em>(4) People studying:</em></p>';
+      // desc += '<p><img src="http://i1246.photobucket.com/albums/gg611/theofficechic/Design/profile-round.png" style="width:20%;height:20%;">\
+      // <img src="http://ablissfulhaven.com/dev/wp-content/uploads/2015/06/round-chokolatta-profile-pic.png" style="width:20%;height:20%;">\
+      // <img src="http://s3-us-west-2.amazonaws.com/s.cdpn.io/6083/profile/profile-512_1.jpg" style="width:20%;height:20%;">\
+      // <img src="https://rigorous-digital.co.uk/wp-content/uploads/2014/06/profile-round.png" style="width:20%;height:20%;"></p>';
+      // desc += '<p class="buttons"><button ion-button style="color:white;background-color:red;">Ask to Join</button></p>';
+      // Leaflet.marker(e.latlng, {icon: profileIcon}).addTo(that.map)
+      //     .bindPopup(desc);//.openPopup();
+      //
+      // Leaflet.circle(e.latlng, radius, {color: 'red'}).addTo(that.map);
     }
-  }
 
-  filter() {
-  }
 
-}
+  }
